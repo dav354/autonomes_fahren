c573d032b788f1796da186db5f249c3c78cf89e6
David <david@glaesle.cloud>
2025-11-03 12:56:04 +0100

    fix flake

--- flake.nix
+++ flake.nix
@@ -2,36 +2,27 @@
   description = "Development environment for the JetBot";
 
   inputs = {
-    nixpkgs-old.url = "github:NixOS/nixpkgs/nixos-24.11";
-    nixpkgs-new.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
-    nixpkgs-old,
-    nixpkgs-new,
+    nixpkgs,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgsOld = import nixpkgs-old {
+      pkgs = import nixpkgs {
         inherit system;
         config = {allowUnfree = true;};
       };
-      pkgsNew = import nixpkgs-new {
-        inherit system;
-        config = {allowUnfree = true;};
-      };
-      pythonEnv = pkgsOld.python36.withPackages (pyPkgs:
-        with pyPkgs; [
-          jupyter-core
-        ]);
     in {
-      devShells.default = pkgsNew.mkShell {
-        packages = with pkgsNew; [
-          pythonEnv
-          black
-          isort
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.python3
+          pkgs.python3Packages.jupyter
+          pkgs.black
+          pkgs.isort
         ];
 
         shellHook = ''
